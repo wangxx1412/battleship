4437b33fa30f027758907118e44dac88576e5d49
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-02-21 16:53:01 -0800

    Complete drag and drop functionality

---
+++ js/fleet/index.js
@@ -0,0 +1,5 @@
+class Carrier {
+  constructor(length) {
+    this.length = length;
+  }
+}
--- js/index.js
+++ js/index.js
@@ -1,37 +1,95 @@
+const field = {
+  carrier: { location: [], rotate: false },
+  battleShip: { location: [], rotate: false },
+  destroyer: { location: [], rotate: false },
+  submarine: { location: [], rotate: false },
+  cruiser: { location: [], rotate: false }
+};
+
+// Drag
 $(function() {
   $(".draggable").draggable({
-    containment: "document",
+    containment: ".field",
     cursor: "move",
     revert: true
   });
 });
 
+// Drop
 $(function() {
   $(".dropsquare").droppable({
     drop: handleDrop
   });
 });
 
+$(function() {
+  $(".fleet")
+    .not(".dropsquare")
+    .droppable({
+      drop: handleDropOut
+    });
+});
+
+// Drop handler
 function handleDrop(event, ui) {
   ui.draggable.position({
     of: $(this),
     my: "left top",
     at: "left top"
   });
+
   ui.draggable.draggable("option", "revert", false);
-  console.log("Dropped");
-  console.log(
-    $(ui.draggable)
-      .attr("class")
-      .split(" ")[0]
-  );
-  console.log(
+
+  const ship = $(ui.draggable)
+    .attr("class")
+    .split(" ")[0];
+
+  if (
     $(this)
       .attr("class")
-      .split(" ")[1]
-  );
+      .split(" ")[0] === "dropsquare"
+  ) {
+    field[ship].location = [
+      $(this)
+        .attr("class")
+        .split(" ")[1]
+    ];
+  }
+  console.log(field);
 }
 
 // Toggle to rotate the ship
+$(".rotate").dblclick(function(event) {
+  const ship = $(this)
+    .attr("class")
+    .split(" ")[0];
 
-// Cant duplicate ships
+  $(this).toggleClass(`${ship}rotated`);
+
+  field[ship].rotate = !field[ship].rotate;
+
+  if (field[ship].rotate) {
+    field[ship].location;
+  } else {
+    field[ship].location;
+  }
+  console.log(field);
+});
+
+function handleDropOut(event, ui) {
+  ui.draggable.position({
+    of: $(this),
+    my: "left top",
+    at: "left top"
+  });
+
+  ui.draggable.draggable("option", "revert", false);
+
+  const ship = $(ui.draggable)
+    .attr("class")
+    .split(" ")[0];
+
+  field[ship].location = [];
+
+  console.log("Out", field);
+}
